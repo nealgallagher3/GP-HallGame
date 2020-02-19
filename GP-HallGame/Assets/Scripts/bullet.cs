25fd64e63e2db0f123d6033c526b4d9dbdb97647
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-02-18 21:27:22 -0500

    Shooting
    Shooting works but have question for Taylor about collision and visual studio

---
+++ GP-HallGame/Assets/Scripts/bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rig;
+   
+    void Start()
+    {
+        rig.velocity = transform.forward * speed;
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.name);
+        Destroy(gameObject);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
